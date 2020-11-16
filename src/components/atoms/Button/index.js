2f72e192c5925baf5e80db405ec449719ac3aa80
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-11-16 16:30:21 +0700

    Berpindah halaman dengan Stack Navigation

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export default function Button({type, children}) {
+export default function Button({type, children, onPress}) {
   return (
-    <View style={styles.container(type)}>
+    <TouchableOpacity style={styles.container(type)} onPress={onPress}>
       <Text style={styles.text(type)}>{children}</Text>
-    </View>
+    </TouchableOpacity>
   );
 }
 
--- src/pages/GetStarted/index.js
+++ src/pages/GetStarted/index.js
@@ -3,7 +3,7 @@ import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 import {ILGetStarted, ILLogo} from '../../assets';
 import {Button, Gap} from '../../components';
 
-export default function GetStarted() {
+export default function GetStarted({navigation}) {
   return (
     <ImageBackground source={ILGetStarted} style={styles.page}>
       <View>
@@ -13,9 +13,13 @@ export default function GetStarted() {
         </Text>
       </View>
       <View style={styles.buttons}>
-        <Button>Get Started</Button>
+        <Button onPress={() => navigation.navigate('Register')}>
+          Get Started
+        </Button>
         <Gap height={16} />
-        <Button type="secondary">Sign In</Button>
+        <Button type="secondary" onPress={() => navigation.replace('Login')}>
+          Sign In
+        </Button>
       </View>
     </ImageBackground>
   );
